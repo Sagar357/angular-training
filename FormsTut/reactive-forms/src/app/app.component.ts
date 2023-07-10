2f268ea771c1f370f8a2d2c8d63f04aafd65a473
Sagar357 <markushno357@gmail.com>
2023-07-10 21:26:46 +0530

    10-07-2023

--- FormsTut/reactive-forms/src/app/app.component.ts
+++ FormsTut/reactive-forms/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactive-forms';
+  public RegistrationForm=new FormGroup(
+    {
+      name:new FormControl('ss'),
+      password:new FormControl(''),
+      confirm_password:new FormControl(''),
+      address:new FormGroup({
+        city:new FormControl(),
+        state:new FormControl(),
+        postal_code: new FormControl()
+      })
+    }
+  );
 }
