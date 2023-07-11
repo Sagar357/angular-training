c9bd98c9c09e85623bb67b39de0726cd58072c98
Sagar357 <markushno357@gmail.com>
2023-07-11 23:31:18 +0530

    Angular Repo 110723

--- FormsTut/reactive-forms/src/app/app.component.ts
+++ FormsTut/reactive-forms/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+//import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormsModule ,Validators} from '@angular/forms';
+import { forbiddenNameValidator } from './Shared/username.validator';
 
 @Component({
   selector: 'app-root',
@@ -8,16 +10,28 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class AppComponent {
   title = 'reactive-forms';
-  public RegistrationForm=new FormGroup(
-    {
-      name:new FormControl('ss'),
-      password:new FormControl(''),
-      confirm_password:new FormControl(''),
-      address:new FormGroup({
-        city:new FormControl(),
-        state:new FormControl(),
-        postal_code: new FormControl()
-      })
-    }
-  );
+  constructor(private fb:FormBuilder){}
+  public RegistrationForm=this.fb.group({
+    name:['Sagar' ,[Validators.required ,Validators.minLength(3),forbiddenNameValidator]],
+    phone:[''],
+    password:[''],
+    confirm_password:[''],
+    address:this.fb.group({
+      city:[''],
+      state:[''],
+      postal_code:['']
+    })
+  })
+  // public RegistrationForm=new FormGroup(
+  //   {
+  //     name:new FormControl('ss'),
+  //     password:new FormControl(''),
+  //     confirm_password:new FormControl(''),
+  //     address:new FormGroup({
+  //       city:new FormControl(),
+  //       state:new FormControl(),
+  //       postal_code: new FormControl()
+  //     })
+  //   }
+  // );
 }
