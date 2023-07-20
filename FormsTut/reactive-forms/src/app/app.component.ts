86247be18e795251a24f0d4eb72a341d777c8475
Sagar357 <markushno357@gmail.com>
2023-07-20 10:04:29 +0530

    dynamic controls

--- FormsTut/reactive-forms/src/app/app.component.ts
+++ FormsTut/reactive-forms/src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 //import { FormControl, FormGroup } from '@angular/forms';
-import { FormBuilder, FormsModule ,Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray ,Validators, Form} from '@angular/forms';
 import { forbiddenName } from './Shared/username.validator';
 
 @Component({
@@ -8,20 +8,39 @@ import { forbiddenName } from './Shared/username.validator';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'reactive-forms';
   constructor(private fb:FormBuilder){}
-  public RegistrationForm=this.fb.group({
-    name:['Sagar' ,[Validators.required ,Validators.minLength(3),forbiddenName(new RegExp('[a^]'))]],
-    phone:[''],
-    password:[''],
-    confirm_password:[''],
-    address:this.fb.group({
-      city:[''],
-      state:[''],
-      postal_code:['']
+  public RegistrationForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.RegistrationForm=this.fb.group({
+      name:['Sagar' ,[Validators.required ,Validators.minLength(3),forbiddenName(new RegExp('[a^]'))]],
+      phone:[''],
+      subscribe:[''],
+      email:[''],
+      password:[''],
+      confirm_password:[''],
+      address:this.fb.group({
+        city:[''],
+        state:[''],
+        postal_code:['']
+      }),
+      alternateEmail:this.fb.array([])
     })
-  })
+    
+    this.RegistrationForm.get('subscribe')?.valueChanges
+      .subscribe(ischecked=>{
+        const emailControl=this.RegistrationForm.get('email');
+        if(ischecked){
+          emailControl?.setValidators(Validators.required);
+        }
+        else{
+          emailControl?.clearValidators();
+        }
+        emailControl?.updateValueAndValidity();
+      });
+  }
   // public RegistrationForm=new FormGroup(
   //   {
   //     name:new FormControl('ss'),
@@ -34,4 +53,26 @@ export class AppComponent {
   //     })
   //   }
   // );
+
+  /**
+   * getUserName
+   * getter method to replace RegistrationForm.get('name') in app.component.html
+   */
+  get getUserName() {
+    return this.RegistrationForm.get('name');
+    
+  }
+
+  get getEmail(){
+    return this.RegistrationForm.get('email');
+  }
+
+  get getAlternateEmail(){
+
+    return this.RegistrationForm.get('alternateEmail') as FormArray;
+  }
+
+  public addAlternateEmails(){
+    this.getAlternateEmail.push( this.fb.control(''));
+  }
 }
